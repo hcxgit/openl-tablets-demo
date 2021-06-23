ce96ee9ff2b6ca01ae6021d49a025ee20c253ea4
hcxgit <haochongxiao@mininglamp.com>
2021-06-23 17:43:11 +0800

    feat: 修改json文件

--- src/main/java/com/comininglamp/openRules/service/MessageTest.java
+++ src/main/java/com/comininglamp/openRules/service/MessageTest.java
@@ -1,13 +1,13 @@
 package com.comininglamp.openRules.service;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
 import com.comininglamp.openRules.beans.Message;
 import lombok.extern.slf4j.Slf4j;
 import org.openl.rules.runtime.RulesEngineFactory;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
 
 /**
@@ -17,54 +17,28 @@ import java.net.URL;
 @Slf4j
 public class MessageTest {
 
-    void callRules() {
+    void callRules() throws IOException {
         URL resource = MessageTest.class.getClassLoader().getResource("rules/messageTest.xlsx");
         System.out.println(resource);
         RulesEngineFactory<Service> engineFactory = new RulesEngineFactory<>(resource, Service.class);
         Service rules = engineFactory.newEngineInstance();
 
-        String path = MessageTest.class.getClassLoader().getResource("rules/message.txt").getPath();
-        JSONArray jsonList = parseJsonFile(new File(path));
-        for (Object js : jsonList) {
-            JSONObject jsonObject = (JSONObject) JSONArray.toJSON(js);
-            Message messageData = JSON.parseObject(jsonObject.toString(), Message.class);
+        BufferedReader bufferedReader =
+                new BufferedReader(new InputStreamReader(MessageTest.class.getClassLoader().getResourceAsStream("rules/message.txt")));
 
-            boolean result = rules.ValidationMessage(messageData);
+        String line = null;
+        while ((line = bufferedReader.readLine()) != null) {
+            Message message = JSON.parseObject(line, Message.class);
+            boolean result = rules.ValidationMessage(message);
             if (result) {
-                System.out.println("-------正确的流量-----:" + messageData.toString());
+                System.out.println("-------正确的流量-----:" + message.toString());
             }
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         MessageTest mess = new MessageTest();
         mess.callRules();
 
-    }
-
-    public static JSONArray parseJsonFile(File filepath) {
-        BufferedReader reader = null;
-        String tempString;
-        StringBuilder content = new StringBuilder();
-        try {
-            FileInputStream file = new FileInputStream(filepath);
-            InputStreamReader inputFileReader = new InputStreamReader(file, "utf-8");
-            reader = new BufferedReader(inputFileReader);
-            while ((tempString = reader.readLine()) != null) {
-                content.append(tempString);
-            }
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-            log.info("解析json文件出错:" + filepath + e);
-        } finally {
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException e1) {
-                }
-            }
-        }
-        return JSON.parseArray(content.toString());
     }
 }
